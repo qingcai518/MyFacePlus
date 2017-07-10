5afd210e282d3445e14dd32d2430a5932fc1d5c4
Qingcan <q_li@fullspeed.co.jp>
2017-07-10 19:04:52 +0900

    other process for image

--- MyFacePlus/FaceManager.swift
+++ MyFacePlus/FaceManager.swift
@@ -43,4 +43,51 @@ class FaceManager {
         
         return blendFilter.outputImage
     }
+    
+//    func makeBarcaFace(with inputImage: CIImage?, _ faceObject: AVMetadataFaceObject?) -> CIImage? {
+//        guard let personciImage = inputImage else {return nil}
+//        guard let faceObject = faceObject else {return nil}
+//        
+//        var persionPic = UIImage(ciImage: personciImage)
+//        
+//        let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+//        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
+//        let faces = faceDetector?.features(in: personciImage)
+//        
+//        // 转换坐标系
+//        let ciImageSize = personciImage.extent.size
+//        var transform = CGAffineTransform(scaleX: 1, y: -1)
+//        transform = transform.translatedBy(x: 0, y: -ciImageSize.height)
+//        
+//        for face in faces as! [CIFaceFeature] {
+//            print("Found bounds are \(face.bounds)")
+//            // 应用变换转换坐标
+//            var faceViewBounds = face.bounds.applying(transform)
+//            // 在图像视图中计算矩形的实际位置和大小
+//            
+////            let viewSize = personPic.bounds.size
+//            let viewSize = CGSize(width: 80, height: 80)
+//            let scale = min(viewSize.width / ciImageSize.width, viewSize.height / ciImageSize.height)
+//            let offsetX = (viewSize.width - ciImageSize.width * scale) / 2
+//            let offsetY = (viewSize.height - ciImageSize.height * scale) / 2
+//            
+//            faceViewBounds = faceViewBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
+//            faceViewBounds.origin.x += offsetX
+//            faceViewBounds.origin.y += offsetY
+//            
+////            let faceBox = UIView(frame: faceViewBounds)
+////            faceBox.layer.borderWidth = 3
+////            faceBox.layer.borderColor = UIColor.red.cgColor
+////            faceBox.backgroundColor = UIColor.clear
+////            personPic.addSubview(faceBox)
+//            
+//            if face.hasLeftEyePosition {
+//                print("Left eye bounds are \(face.leftEyePosition)")
+//            }
+//            
+//            if face.hasRightEyePosition {
+//                print("Right eye bounds are \(face.rightEyePosition)")
+//            }
+//        }
+//    }
 }
