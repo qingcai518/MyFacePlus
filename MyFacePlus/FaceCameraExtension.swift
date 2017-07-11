1fb1bbcfa7f8892f019c8df83da92aa4297bc97a
Qingcan <q_li@fullspeed.co.jp>
2017-07-11 14:37:09 +0900

    add gesture for BarcaView

--- MyFacePlus/FaceCameraExtension.swift
+++ MyFacePlus/FaceCameraExtension.swift
@@ -42,17 +42,17 @@ extension FaceCameraController: AVCaptureVideoDataOutputSampleBufferDelegate {
         DispatchQueue.main.async {
             self.previewLayer.contents = self.context.createCGImage(ciImage, from: ciImage.extent)
             // 他の効果を選択した場合
-            self.addEffective(self.viewModel.faceType, ciImage)
+            self.addEffective(self.viewModel.faceType)
         }
     }
     
     /**
      * FaceTypeにより、効果を追加する.
      */
-    private func addEffective(_ type: FaceType, _ ciImage: CIImage?) {
+    private func addEffective(_ type: FaceType) {
         switch type {
         case .barca:
-            addBarcaView(ciImage)
+            addBarcaView()
         default:
             removeBarcaView()
         }
@@ -61,9 +61,7 @@ extension FaceCameraController: AVCaptureVideoDataOutputSampleBufferDelegate {
     /**
      * 種類2の効果 : BarcaView.
      */
-    private func addBarcaView(_ ciImage : CIImage?) {
-        guard let inputImage = ciImage else {return}
-        
+    private func addBarcaView() {
         // 顔を検知されなかった場合.
         guard let faceObject = faceObject else {
             removeBarcaView()
@@ -77,19 +75,7 @@ extension FaceCameraController: AVCaptureVideoDataOutputSampleBufferDelegate {
         let width = view.bounds.size.width * faceObject.bounds.size.height
         let height = view.bounds.size.height * faceObject.bounds.size.width
         
-//        let position = CGPoint(x: view.bounds.width * (1 - faceObject.bounds.origin.y - faceObject.bounds.size.height / 2), y: view.bounds.height * (faceObject.bounds.origin.x + faceObject.bounds.size.width / 2))
-//        
-//        let size = CGSize(width: faceObject.bounds.size.height * view.bounds.width, height: faceObject.bounds.size.width * view.bounds.height)
-        
         print("originX = \(originX), originY = \(originY), width = \(width), height = \(height)")
-        
-        // 其他算法
-//        let centerX = view.bounds.size.width * (faceObject.bounds.origin.x + faceObject.bounds.size.width / 2)
-//        let centerY = view.bounds.size.height * (1 - faceObject.bounds.origin.y - faceObject.bounds.size.height / 2)
-//        let radius = faceObject.bounds.size.width * view.bounds.size.width / 2
-//        
-//        print("centerX = \(centerX), centerY = \(centerY), radius = \(radius)")
-        
         
         // 顔を検知された場合.
         if barcaView == nil, let tempView = UINib(nibName: "BarcaView", bundle: nil).instantiate(withOwner: self, options: nil).first as? BarcaView {
@@ -97,6 +83,12 @@ extension FaceCameraController: AVCaptureVideoDataOutputSampleBufferDelegate {
             self.view.addSubview(barcaView)
         }
         barcaView.setIconPosition(originX, originY, width, height)
+        
+        let recognizer = UITapGestureRecognizer()
+        recognizer.rx.event.bind { [weak self] sender in
+            self?.viewModel.mode.value = .normal
+        }.addDisposableTo(disposeBag)
+        barcaView.addGestureRecognizer(recognizer)
     }
     
     private func removeBarcaView() {
